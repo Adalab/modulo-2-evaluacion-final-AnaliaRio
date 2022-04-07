39c087f6eb4862bdb31ec1b8356aa3d1d7683738
Analía Río <analia.rio@gmail.com>
2022-04-07 19:22:47 +0200

    Fetch

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 "use strict";
 
+// HTML elements
 const input = document.querySelector(".js-input");
 const searchBtn = document.querySelector(".js-search");
 const resetBtn = document.querySelector(".reset");
+const list = document.querySelector(".js-list");
+
+// User value
+const inputValue = input.value;
+
+// Data source
 const urlServer = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+
+// Fetch data
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  .then(response => response.json())
+  .then(data => document.body.innerHTML = data.result.value);
+
+
+
+  searchBtn.addEventListener.apply("click",handleSearchBtnClick);
