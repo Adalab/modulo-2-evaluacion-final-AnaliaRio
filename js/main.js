216bb588a6d89f60e4f511eb195145c5e4cb82cf
Analía Río <analia.rio@gmail.com>
2022-04-07 16:39:33 +0200

    JS Constants

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@
 const input = document.querySelector(".js-input");
 const searchBtn = document.querySelector(".js-search");
 const resetBtn = document.querySelector(".reset");
-
+const urlServer = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
