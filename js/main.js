d91324b1c93eab5340f0eb23efa741bfc37bce74
Analía Río <analia.rio@gmail.com>
2022-04-08 09:29:28 +0200

    Search & Reset buttons

--- js/main.js
+++ js/main.js
@@ -1,22 +1,67 @@
 "use strict";
 
-// HTML elements
+// C - Input
 const input = document.querySelector(".js-input");
+// C- Search Button
 const searchBtn = document.querySelector(".js-search");
-const resetBtn = document.querySelector(".reset");
-const list = document.querySelector(".js-list");
-
-// User value
+// C - Reset Btn
+const resetBtn = document.querySelector(".js-reset");
+// C - List
+const renderizedList = document.querySelector(".js-list");
+// C - Input Value
 const inputValue = input.value;
 
-// Data source
-const urlServer = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
 
-// Fetch data
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+// FETCH
+let drinksList = [];
+
+function getData () {
+const inputValue = input.value;
+
+ fetch(
+     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
   .then(response => response.json())
-  .then(data => document.body.innerHTML = data.result.value);
+  .then(data => {
+    drinksList = data.drinks;
+    renderList(drinksList);
+  }); 
+}
+
+// RENDER DRINK LIST IN HTML
+function renderList (listDrinks) {
+    for (const drink of listDrinks) {
+        renderizedList.innerHTML +=  `<li><h2>${drink.strDrink}</h2><img src=${drink.strDrinkThumb} class="img" alt="cocktail"></li>`;
+    }
+};
+
+function handleSearchBtnClick(event) {
+    event.preventDefault();
+    getData();
+}
+
+searchBtn.addEventListener("click",handleSearchBtnClick);
+
+
+// RESET RENDERIZED DRINK LIST
+function resetList () {
+    drinksList = [];
+    renderizedList.innerHTML = "";
+};
+
+function handleResetBtnClick(event) {
+    event.preventDefault();
+    resetList();
+}
+
+resetBtn.addEventListener("click",handleResetBtnClick);
 
 
 
-  searchBtn.addEventListener.apply("click",handleSearchBtnClick);
+
+
+
+
+
+
+
+
